e6c0072d890f2ceb0deeca8dcdda0c9e2961bfb3
Matthew Johnston <warmwaffles@gmail.com>
2023-11-28 11:11:58 -0600

    Update locked dependencies to their latest

--- test/ex_image_info_test/images/bmp_test.exs
+++ test/ex_image_info_test/images/bmp_test.exs
@@ -14,7 +14,7 @@ defmodule ExImageInfoTest.Images.BMPTest do
   test "force - bmp disk image - #seems? #type #info", images do
     assert seems?(images["bmp"], :bmp) == true
     assert type(images["bmp"], :bmp) == {"image/bmp", "BMP"}
-    assert info(images["bmp"], :bmp) == {"image/bmp",130, 42, "BMP"}
+    assert info(images["bmp"], :bmp) == {"image/bmp", 130, 42, "BMP"}
   end
 
   # @tag :wip
