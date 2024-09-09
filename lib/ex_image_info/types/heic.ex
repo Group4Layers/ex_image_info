d031617cb6cd10c41344dba6a051d51abf2f8a37
bu6n <b@u6n.ch>
2024-09-09 11:09:26 +0200

    feat: add support for the AVIF, HEIC and HEIF formats
    - These 3 formats have in common that they use the HEIF container format specified in ISO/IEC 23008-12.
    - The MIME types and variant types (file extension) for HEIC and HEIF are based on sections C.2 and D.2 of the specification.
    - A HEIF container can contain multiple images, so the logic is kept as in `:ico` (take the size of the largest image).
    - The size of an image is found in the `ispe` box.
    
    style: applying styler before rebasing

---
+++ lib/ex_image_info/types/heic.ex
@@ -0,0 +1,80 @@
+defmodule ExImageInfo.Types.HEIC do
+  @moduledoc false
+
+  @behaviour ExImageInfo.Detector
+
+  alias ExImageInfo.Types.HEIF
+
+  @mime_heic "image/heic"
+  @mime_heic_sequence "image/heic-sequence"
+  @signature_heic <<"ftypheic">>
+  @signature_heim <<"ftypheim">>
+  @signature_heis <<"ftypheis">>
+  @signature_heix <<"ftypheix">>
+  @signature_hevc <<"ftyphevc">>
+  @signature_hevm <<"ftyphevm">>
+  @signature_hevs <<"ftyphevs">>
+  @signature_hevx <<"ftyphevx">>
+  @ftype_heic "HEIC"
+  @ftype_heic_sequence "HEICS"
+
+  ## Public API
+
+  def seems?(<<_::size(32), @signature_heic, _rest::binary>>), do: true
+  def seems?(<<_::size(32), @signature_heim, _rest::binary>>), do: true
+  def seems?(<<_::size(32), @signature_heis, _rest::binary>>), do: true
+  def seems?(<<_::size(32), @signature_heix, _rest::binary>>), do: true
+  def seems?(_), do: false
+
+  def info(<<_::size(32), @signature_heic, rest::binary>>),
+    do: HEIF.info(rest, @mime_heic, @ftype_heic)
+
+  def info(<<_::size(32), @signature_heim, rest::binary>>),
+    do: HEIF.info(rest, @mime_heic, @ftype_heic)
+
+  def info(<<_::size(32), @signature_heis, rest::binary>>),
+    do: HEIF.info(rest, @mime_heic, @ftype_heic)
+
+  def info(<<_::size(32), @signature_heix, rest::binary>>),
+    do: HEIF.info(rest, @mime_heic, @ftype_heic)
+
+  def info(<<_::size(32), @signature_hevc, rest::binary>>),
+    do: HEIF.info(rest, @mime_heic_sequence, @ftype_heic_sequence)
+
+  def info(<<_::size(32), @signature_hevm, rest::binary>>),
+    do: HEIF.info(rest, @mime_heic_sequence, @ftype_heic_sequence)
+
+  def info(<<_::size(32), @signature_hevs, rest::binary>>),
+    do: HEIF.info(rest, @mime_heic_sequence, @ftype_heic_sequence)
+
+  def info(<<_::size(32), @signature_hevx, rest::binary>>),
+    do: HEIF.info(rest, @mime_heic_sequence, @ftype_heic_sequence)
+
+  def info(_), do: nil
+
+  def type(<<_::size(32), @signature_heic, _rest::binary>>),
+    do: {@mime_heic, @ftype_heic}
+
+  def type(<<_::size(32), @signature_heim, _rest::binary>>),
+    do: {@mime_heic, @ftype_heic}
+
+  def type(<<_::size(32), @signature_heis, _rest::binary>>),
+    do: {@mime_heic, @ftype_heic}
+
+  def type(<<_::size(32), @signature_heix, _rest::binary>>),
+    do: {@mime_heic, @ftype_heic}
+
+  def type(<<_::size(32), @signature_hevc, _rest::binary>>),
+    do: {@mime_heic_sequence, @ftype_heic_sequence}
+
+  def type(<<_::size(32), @signature_hevm, _rest::binary>>),
+    do: {@mime_heic_sequence, @ftype_heic_sequence}
+
+  def type(<<_::size(32), @signature_hevs, _rest::binary>>),
+    do: {@mime_heic_sequence, @ftype_heic_sequence}
+
+  def type(<<_::size(32), @signature_hevx, _rest::binary>>),
+    do: {@mime_heic_sequence, @ftype_heic_sequence}
+
+  def type(_), do: nil
+end
---
+++ lib/ex_image_info/types/heif.ex
@@ -0,0 +1,57 @@
+defmodule ExImageInfo.Types.HEIF do
+  @moduledoc false
+
+  @behaviour ExImageInfo.Detector
+
+  @mime_heif "image/heif"
+  @mime_heif_sequence "image/heif-sequence"
+  @signature_mif1 <<"ftypmif1">>
+  @signature_msf1 <<"ftypmsf1">>
+  @ftype_heif "HEIF"
+  @ftype_heif_sequence "HEIFS"
+
+  ## Public API
+
+  def seems?(<<_::size(32), @signature_mif1, _rest::binary>>), do: true
+  def seems?(<<_::size(32), @signature_msf1, _rest::binary>>), do: true
+  def seems?(_), do: false
+
+  def info(<<_::size(32), @signature_mif1, rest::binary>>) do
+    info(rest, @mime_heif, @ftype_heif)
+  end
+
+  def info(<<_::size(32), @signature_msf1, rest::binary>>),
+    do: info(rest, @mime_heif_sequence, @ftype_heif_sequence)
+
+  def info(_), do: nil
+
+  def info(binary, mime, ftype) do
+    case size(binary) do
+      {w, h} -> {mime, w, h, ftype}
+      _ -> nil
+    end
+  end
+
+  def type(<<_::size(32), @signature_mif1, _rest::binary>>),
+    do: {@mime_heif, @ftype_heif}
+
+  def type(<<_::size(32), @signature_msf1, _rest::binary>>),
+    do: {@mime_heif_sequence, @ftype_heif_sequence}
+
+  def type(_), do: nil
+
+  defp size(binary) do
+    [_hd | ispe_boxes] = String.split(binary, "ispe")
+
+    if length(ispe_boxes) > 0 do
+      {width, height} =
+        ispe_boxes
+        |> Enum.map(fn <<_::size(32), width::size(32), height::size(32), _rest::binary>> ->
+          {width, height}
+        end)
+        |> Enum.max_by(&elem(&1, 0))
+
+      {width, height}
+    end
+  end
+end
