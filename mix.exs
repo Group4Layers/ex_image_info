b02aa13fba4bdb734538b9f43ed254178df0b507
rNoz <rnoz.commits@gmail.com>
2025-03-05 19:25:00 +0100

    chore: updating all packages and bumping versions (styler, ex_doc)

--- mix.exs
+++ mix.exs
@@ -33,10 +33,10 @@ defmodule ExImageInfo.Mixfile do
   defp deps do
     [
       {:excoveralls, "~> 0.18", only: :test},
-      {:ex_doc, "~> 0.30", only: :dev},
+      {:ex_doc, "~> 0.37", only: :dev},
       {:inch_ex, "~> 2.0", only: [:dev, :test]},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
-      {:styler, "~> 1.3", only: [:dev, :test], runtime: false}
+      {:styler, "~> 1.4", only: [:dev, :test], runtime: false}
     ]
   end
 
