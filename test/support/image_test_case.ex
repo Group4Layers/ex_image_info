3324333470b9ff229ae31f65ad65f911a7667f29
rNoz <rnoz.commits@gmail.com>
2025-03-27 14:30:44 +0100

    test: refactored to avoid double importing ExCase/CaseTemplate

--- test/support/image_test_case.ex
+++ test/support/image_test_case.ex
@@ -1,16 +1,19 @@
 defmodule ImageTestCase do
   @moduledoc false
-  use ExUnit.CaseTemplate, async: true
+
+  defmacro __using__(opts) do
+    quote do
+      use ExUnit.Case, async: async?
+
+      import ImageTestCase
+
+      async? = Keyword.get(unquote(opts), :async, true)
+    end
+  end
 
   @doc "Reads an image giving a relative path to test/fixtures/images"
   def read_image(rel_path) do
     abs_path = Path.expand(Path.join("test/fixtures/images", rel_path), File.cwd!())
     File.read!(abs_path)
-  end
-
-  using do
-    quote do
-      def read_image(rel_path), do: ImageTestCase.read_image(rel_path)
-    end
   end
 end
